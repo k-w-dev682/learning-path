58035e574c8ec34ff832d80662e114020c59e233
k-w-dev682 <kacper.wozniak034@gmail.com>
2026-05-28 12:15:26 +0200

    Cpp learning - typedef/using

---
+++ Projects/Cpp/Full Course for free/helloworld.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+
+//typedef std::vector<std::pair<std::string, int>> pairlist_t;
+//typedef std::string text_t;
+//typedef int number_t;
+using text_t = std::string;
+using number_t = int;
+
+int main() {
+    text_t firstName = "COS";
+    number_t age = 20;
+    std::cout << firstName << '\n';
+    std::cout << age << '\n';
+
+}
