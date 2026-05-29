fd615be6854914fe05788b392652e72d4f58e0ab
k-w-dev682 <kacper.wozniak034@gmail.com>
2026-05-29 11:11:57 +0200

    Cpp tutorial - input

--- Projects/Cpp/Full Course for free/helloworld.cpp
+++ Projects/Cpp/Full Course for free/helloworld.cpp
@@ -1,16 +1,9 @@
 #include <iostream>
 #include <vector>
 
-//typedef std::vector<std::pair<std::string, int>> pairlist_t;
-//typedef std::string text_t;
-//typedef int number_t;
-using text_t = std::string;
-using number_t = int;
-
 int main() {
-    text_t firstName = "COS";
-    number_t age = 20;
-    std::cout << firstName << '\n';
-    std::cout << age << '\n';
-
+    std::string name;
+    std::cout<<"What's your full name?: ";
+    std::getline(std::cin, name);
+    std::cout<<name;
 }
