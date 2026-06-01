2ef6165383211b621435ab56467a3dafbfd15263
k-w-dev682 <kacper.wozniak034@gmail.com>
2026-06-01 10:40:42 +0200

    Temperature conversion program

--- Projects/Cpp/Full Course for free/helloworld.cpp
+++ Projects/Cpp/Full Course for free/helloworld.cpp
@@ -2,15 +2,32 @@
 #include <vector>
 
 int main() {
-    //int grade = 55;
-   // grade >= 60 ? std::cout << "You pass!" : std::cout << "You fail";
+    double temp;
+    char unit;
 
-   //int number = 9;
-   //number % 2 ? std::cout << "ODD" : std::cout << "EVEN";
+    std::cout << "***** Tempterature conversion *****\n";
+    std::cout << "F = Fahrenheit\n";
+    std::cout << "C = Celsius\n";
+    std::cout << "What unit would you  like to convert to: ";
+    std::cin >> unit;
+    
+    if(unit == 'F' || unit == 'f') {
+        std::cout << "Enter the temperature in Celsius: ";
+        std::cin >> temp;
 
-   bool hungry = false;
-   //hungry ? std::cout << "You are hungry" : std::cout << "You are full";
-   std::cout << (hungry ? "You are hungry" : "You are full");
+        temp = (1.8 * temp) +32.0;
+        std::cout << "Temperature is: " << temp << "F\n";
+    }
+    else if (unit == 'C' || unit == 'c') {
+        std::cout << "Enter the temperature in Celsius: ";
+        std::cin >> temp;
+
+        temp = (temp - 32) / 1.8;
+        std::cout << "Temperature is: " << temp << "C\n";
+
+    } else {
+        std::cout << "Please enter only C or F\n";
+    }
 
     return 0;
 }
