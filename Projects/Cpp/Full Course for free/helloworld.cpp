088d4d8f46dd61bb8517f83131bbb64a19a8b13f
k-w-dev682 <kacper.wozniak034@gmail.com>
2026-06-01 10:32:08 +0200

    Ternary operator

--- Projects/Cpp/Full Course for free/helloworld.cpp
+++ Projects/Cpp/Full Course for free/helloworld.cpp
@@ -2,9 +2,15 @@
 #include <vector>
 
 int main() {
-    
-    int grade = 55;
-    grade >= 60 ? std::cout << "You pass!" : std::cout << "You fail";
-    
+    //int grade = 55;
+   // grade >= 60 ? std::cout << "You pass!" : std::cout << "You fail";
+
+   //int number = 9;
+   //number % 2 ? std::cout << "ODD" : std::cout << "EVEN";
+
+   bool hungry = false;
+   //hungry ? std::cout << "You are hungry" : std::cout << "You are full";
+   std::cout << (hungry ? "You are hungry" : "You are full");
+
     return 0;
 }
