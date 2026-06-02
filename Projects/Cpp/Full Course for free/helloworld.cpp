e4f701391ee110925d0fefcefeb191a2299dbf7f
k-w-dev682 <kacper.wozniak034@gmail.com>
2026-06-02 11:28:09 +0200

    break and continue keywords

--- Projects/Cpp/Full Course for free/helloworld.cpp
+++ Projects/Cpp/Full Course for free/helloworld.cpp
@@ -2,9 +2,13 @@
 #include <vector>
 
 int main() {
-    for(int i = 10; i >= 0; i-=2) {
-        std::cout << i <<'\n';
+    for(int i = 1; i <= 20; i++) {
+        if(i == 13) {
+            // break;
+            continue;
+        }
+        std::cout << i << '\n';
     }
-    std::cout << "HAPPY NEW YEAR!\n";
+
     return 0;
 }
