6c83c8d88e99d0752c743f22fecaaa59e28ebb77
k-w-dev682 <kacper.wozniak034@gmail.com>
2026-06-02 11:26:15 +0200

    For loop

--- Projects/Cpp/Full Course for free/helloworld.cpp
+++ Projects/Cpp/Full Course for free/helloworld.cpp
@@ -2,14 +2,9 @@
 #include <vector>
 
 int main() {
-    int number;
-
-    do{
-        std::cout << "Enter a positive #: ";
-        std::cin >> number;
-    } while(number < 0);
-
-    std::cout << "The # is: " << number;
-
+    for(int i = 10; i >= 0; i-=2) {
+        std::cout << i <<'\n';
+    }
+    std::cout << "HAPPY NEW YEAR!\n";
     return 0;
 }
