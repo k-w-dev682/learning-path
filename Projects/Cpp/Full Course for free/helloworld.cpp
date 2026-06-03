c8c931f7e7309f5ff43001e4315adfbb23d0c494
k-w-dev682 <kacper.wozniak034@gmail.com>
2026-06-03 12:54:46 +0200

    Overloaded functions

--- Projects/Cpp/Full Course for free/helloworld.cpp
+++ Projects/Cpp/Full Course for free/helloworld.cpp
@@ -1,39 +1,21 @@
 #include <iostream>
 
-/*
-double square(double length);
-double cube(double length);
+void bakePizza();
+void bakePizza(std::string topping1);
+void bakePizza(std::string topping1, std::string topping2);
 
 int main() {
-    double length = 6.0;
-    double area = square(length);
-    double volume = cube(length);
-
-    std::cout << "Area: " << area << "cm^2\n";
-    std::cout << "Volume: " << volume << "cm^3\n";
+    bakePizza("pepperoni", "mushrooms");
 
     return 0;
 }
 
-double square(double length) {
-    return length * length;
+void bakePizza() {
+    std::cout << "Here is your Pizza!\n";
 }
-double cube(double length) {
-    return length * length * length;
-} */
-
-std::string concatStrings(std::string string1, std::string string2);
-
-int main() {\
-    std::string firstName = "AA";
-    std::string lastName = "BB";
-    std::string fullName = concatStrings(firstName, lastName);
-
-    std::cout << "Hello " << fullName;
-
-    return 0;
+void bakePizza(std::string topping1) {
+    std::cout << "Here is your " << topping1 << " pizza!\n";
 }
-
-std::string concatStrings(std::string string1, std::string string2) {
-    return string1 + " " + string2;
+void bakePizza(std::string topping1, std::string topping2) {
+    std::cout << "Here is your " << topping1 << " and " << topping2 << " pizza!\n";
 }
