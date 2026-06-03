23cedbb04dc1eb11f0477f5fb4ae0960b54c8eb4
k-w-dev682 <kacper.wozniak034@gmail.com>
2026-06-03 12:50:37 +0200

    Return keyword

--- Projects/Cpp/Full Course for free/helloworld.cpp
+++ Projects/Cpp/Full Course for free/helloworld.cpp
@@ -1,20 +1,39 @@
 #include <iostream>
 
-void happyBirthday(std::string name, int age);
+/*
+double square(double length);
+double cube(double length);
 
 int main() {
-    std::string name = "dsadsa";
-    int age = 21;
+    double length = 6.0;
+    double area = square(length);
+    double volume = cube(length);
 
-    happyBirthday(name, age);
+    std::cout << "Area: " << area << "cm^2\n";
+    std::cout << "Volume: " << volume << "cm^3\n";
 
     return 0;
 }
 
-void happyBirthday(std::string name, int age) {
-    std::cout << "Happy Birthday to "<< name << "\n";
-    std::cout << "Happy Birthday to "<< name << "\n";
-    std::cout << "Happy Birthday to "<< name << "\n";
-    std::cout << "Happy Birthday to "<< name << "\n";
-    std::cout << "You are "<< age << " years old" << "\n";
+double square(double length) {
+    return length * length;
+}
+double cube(double length) {
+    return length * length * length;
+} */
+
+std::string concatStrings(std::string string1, std::string string2);
+
+int main() {\
+    std::string firstName = "AA";
+    std::string lastName = "BB";
+    std::string fullName = concatStrings(firstName, lastName);
+
+    std::cout << "Hello " << fullName;
+
+    return 0;
+}
+
+std::string concatStrings(std::string string1, std::string string2) {
+    return string1 + " " + string2;
 }
