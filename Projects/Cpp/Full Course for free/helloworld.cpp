f1d652bd4542dbeb3ec9e3d4d79830d2acfe0387
k-w-dev682 <kacper.wozniak034@gmail.com>
2026-06-04 15:08:48 +0200

    Banking practice program

--- Projects/Cpp/Full Course for free/helloworld.cpp
+++ Projects/Cpp/Full Course for free/helloworld.cpp
@@ -1,17 +1,86 @@
 #include <iostream>
+#include <iomanip>
 
-int myNum = 3;
-
-void printNum();
+void showBalance(double balance);
+double deposit();
+double withdraw(double balance);
+int getIntegerInput();
 
 int main() {
-    int myNum = 1;
-    printNum();
-    std::cout << ::myNum << '\n';
+    double balance = 0;
+    int choice = 0;
+
+    do{
+        std::cout << "*******************\n";
+        std::cout << "Enter your choice: \n";
+        std::cout << "*******************\n";
+        std::cout << "1. Show Balance \n";
+        std::cout << "2. Deposit Money \n";
+        std::cout << "3. Withdraw Money \n";
+        std::cout << "4. Exit \n";
+
+        choice = getIntegerInput();
+
+        switch(choice) {
+            case 1: showBalance(balance);
+                    break;
+            case 2: balance += deposit();
+                    showBalance(balance);
+                    break;
+            case 3: balance -= withdraw(balance);
+                    showBalance(balance);
+                    break;
+            case 4: std::cout << "Thanks for visiting!\n";
+                    break;
+            default: std::cout << "Invalid choice\n";
+        } 
+    } while (choice != 4);
 
     return 0;
 }
-void printNum() {
-    int myNum = 2;
-    std::cout << ::myNum << '\n';
+
+void showBalance(double balance) {
+    std::cout << "Your balance is: $"<< std::setprecision(2) << std::fixed << balance << '\n';
+}
+
+double deposit()  {
+    double amount = 0;
+    std::cout << "Enter amount to be deposited: ";
+    std::cin >> amount;
+
+    if(amount > 0) {
+        return amount;
+    } else {
+        std::cout << "That's not a valid amount: \n";
+        return 0;
+    }
+}
+
+double withdraw(double balance) {
+    double amount = 0;
+
+    std::cout << "Enter amount to be withdrawn: ";
+    std::cin >> amount;
+    
+    if(amount > balance) {
+        std::cout << "Insufficient funds! \n";
+        return 0;
+    }
+    else if(amount < 0) {
+        std::cout << "That's not a valid amount\n";
+        return 0;
+    } else {
+        return amount;
+    }
+    return amount;
+}
+
+int getIntegerInput() {
+    int input;
+    while (!(std::cin >> input)) {
+        std::cin.clear(); 
+        std::cin.ignore(10000, '\n'); 
+        std::cout << "Invalid input! Please enter a number: ";
+    }
+    return input;
 }
