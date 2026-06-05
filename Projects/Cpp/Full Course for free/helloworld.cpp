e44ae814c80a368e05921b209876fca16b96af67
k-w-dev682 <kacper.wozniak034@gmail.com>
2026-06-05 13:15:26 +0200

    sizeof operator

--- Projects/Cpp/Full Course for free/helloworld.cpp
+++ Projects/Cpp/Full Course for free/helloworld.cpp
@@ -1,22 +1,19 @@
 #include <iostream>
 
 int main() {
-    //std::string cars[] = {"Corvertte", "Mustang", "Camry"};
+    std::string name = "Aaa";
+    double gpa = 2.5;
+    char grade = 'F';
+    bool student = true;
+    char grades[] = {'A', 'B', 'C', 'D', 'F'};
+    std::string students[] = {"Spongebob", "Patrick", "Squidward", "Sandy"};
 
-    /* std::string cars[3];
-
-    cars[0] = "Camaro";
-    cars[1] = "Mustang";
-    cars[2] = "Camry";
-
-    std::cout << cars[0] <<'\n';
-    std::cout << cars[1] <<'\n';
-    std::cout << cars[2] <<'\n'; */
-
-    double prices[] = {5.00, 7.50, 9.99, 15.00};
-
-    std::cout << prices[0] << '\n';
-    std::cout << prices[1] << '\n';
-    std::cout << prices[2] << '\n';
-    std::cout << prices[3] << '\n';
+    std::cout << sizeof(gpa) << " bytes\n";
+    std::cout << sizeof(name) << " bytes\n";
+    std::cout << sizeof(grade) << " bytes\n";
+    std::cout << sizeof(student) << " bytes\n";
+    std::cout << sizeof(grades) << " bytes\n";
+    std::cout << sizeof(grades)/sizeof(char) << " elements\n";
+    std::cout << sizeof(students)/sizeof(std::string) << " elements\n";
+    return 0;
 }
