62b1753852c13635a0778d1dcc36cb091a8ea079
k-w-dev682 <kacper.wozniak034@gmail.com>
2026-06-06 13:09:15 +0200

    iterate over an array

--- Projects/Cpp/Full Course for free/helloworld.cpp
+++ Projects/Cpp/Full Course for free/helloworld.cpp
@@ -1,19 +1,12 @@
 #include <iostream>
 
 int main() {
-    std::string name = "Aaa";
-    double gpa = 2.5;
-    char grade = 'F';
-    bool student = true;
+    //std::string students[] = {"Spongebob", "Patrick", "Squidward", "Sandy"};
     char grades[] = {'A', 'B', 'C', 'D', 'F'};
-    std::string students[] = {"Spongebob", "Patrick", "Squidward", "Sandy"};
 
-    std::cout << sizeof(gpa) << " bytes\n";
-    std::cout << sizeof(name) << " bytes\n";
-    std::cout << sizeof(grade) << " bytes\n";
-    std::cout << sizeof(student) << " bytes\n";
-    std::cout << sizeof(grades) << " bytes\n";
-    std::cout << sizeof(grades)/sizeof(char) << " elements\n";
-    std::cout << sizeof(students)/sizeof(std::string) << " elements\n";
+    for(int i = 0; i < sizeof(grades)/sizeof(char); i++) {
+        std::cout << grades[i] << '\n';
+    }
+
     return 0;
 }
