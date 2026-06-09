f04f152d4a7cda6ddeecb051204867d941225da3
k-w-dev682 <kacper.wozniak034@gmail.com>
2026-06-09 11:23:17 +0200

    quiz game

--- Projects/Cpp/Full Course for free/helloworld.cpp
+++ Projects/Cpp/Full Course for free/helloworld.cpp
@@ -1,28 +1,48 @@
 #include <iostream>
 
 int main() {
-    std::string cars[][3] = {{"Mustang", "Escape", "F-150"},
-                            {"Corvette", "Equinox", "Silverado"},
-                            {"Challenger", "Durango", "Ram 1500"}};
+    std::string questions[] =  {"1. What year was C++ created?: ",
+                                "2. Who invented C++?: ", 
+                                "3. What is the predecessor of C++?: ", 
+                                "4, Is the Earth flat?: "};
+    
+    std::string options[][4] =  {{"A. 1969", "B. 1975", "C. 1985", "D. 1989"},
+                                {"A. Guido van Rossum", "B. Bjarne Stroustrup", "C. Jonh Carmack", "D. Mark Zuckerburg"},
+                                {"A. C", "B. C+", "C. C--", "D. B++"},
+                                {"A. yes", "B. no", "C. sometimes", "D. what's Earth?"}};
 
-    /* std::cout << cars[0][0] << " ";
-    std::cout << cars[0][1] << " ";
-    std::cout << cars[0][2] << "\n";
-    std::cout << cars[1][0] << " ";
-    std::cout << cars[1][1] << " ";
-    std::cout << cars[1][2] << "\n";
-    std::cout << cars[2][0] << " ";
-    std::cout << cars[2][1] << " ";
-    std::cout << cars[2][2] << "\n"; */
+    char answerKey[] = {'C', 'B', 'A', 'B'};
 
-    int rows = sizeof(cars)/sizeof(cars[0]);
-    int columns = sizeof(cars[0])/sizeof(cars[0][0]);
+    int size = sizeof(questions)/sizeof(questions[0]);
+    char guess;
+    int score = 0;
 
-    for(int i = 0; i < rows; i++) {
-        for(int j = 0; j < columns; j++) {
-            std::cout << cars[i][j] << " ";
+    for(int i = 0; i < size; i++) {
+        std::cout << "*******************************\n";
+        std::cout << questions[i] << "\n";
+        std::cout << "*******************************\n";
+
+        for(int j = 0; j < sizeof(options[i])/sizeof(options[i][0]); j++) {
+            std::cout << options[i][j] << '\n';
         }
-        std::cout << '\n';
+
+        std::cin >> guess;
+        guess = toupper(guess);
+
+        if(guess == answerKey[i]) {
+            std::cout << "CORRECT\n";
+            score++;
+        } else {
+            std::cout << "WRONG\n";
+            std::cout << "Answer: " << answerKey[i] << '\n';
+        }
     }
+    std::cout << "*******************************\n";
+    std::cout << "*           RESULTS\n         *\n";
+    std::cout << "*******************************\n";
+    std::cout << "CORRECT GUESSES: " << score << '\n';
+    std::cout << "# of QUESTIONS " << size << '\n';
+    std::cout << "SCORE: " << (score/(double)size)*100 << "%";
+
     return 0;
 }
