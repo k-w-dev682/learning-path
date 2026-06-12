ab16b8d06e47267afae04dcdbb596e2c193a4aaa
k-w-dev682 <kacper.wozniak034@gmail.com>
2026-06-12 14:28:02 +0200

    Structs

--- Projects/Cpp/Full Course for free/helloworld.cpp
+++ Projects/Cpp/Full Course for free/helloworld.cpp
@@ -1,13 +1,36 @@
 #include <iostream>
-template <typename T, typename U>
 
-auto max(T x, U y) {
-    return (x > y) ? x : y;
-}
+struct student {
+    std::string name;
+    double gpa;
+    bool enrolled = true;
+};
 
 int main() {
-    std::cout << max(1, 2.1) << '\n';
+    student student1;
+    student1.name = "Spongebob";
+    student1.gpa = 3.2;
 
+    student student2;
+    student2.name = "Patrick";
+    student2.gpa = 2.1;
+
+    student student3;
+    student3.name = "Squidward";
+    student3.gpa = 1.5;
+
+    std::cout << student1.name << '\n';
+    std::cout << student1.gpa << '\n';
+    std::cout << student1.enrolled << '\n';
+
+    std::cout << student2.name << '\n';
+    std::cout << student2.gpa << '\n';
+    std::cout << student2.enrolled << '\n';
+
+    std::cout << student3.name << '\n';
+    std::cout << student3.gpa << '\n';
+    std::cout << student3.enrolled << '\n';
+    
     return 0;
 }
 
