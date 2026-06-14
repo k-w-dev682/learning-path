af55fec59a0dc910b7c8472abbca405e732772da
k-w-dev682 <kacper.wozniak034@gmail.com>
2026-06-14 14:36:10 +0200

    inheritance

--- Projects/Cpp/Full Course for free/helloworld.cpp
+++ Projects/Cpp/Full Course for free/helloworld.cpp
@@ -1,33 +1,71 @@
 #include <iostream>
 
-class Stove {
-    private:
-        int temperature = 0;
+class Animal {
     public:
-    Stove(int temperature) {
-        setTemperature(temperature);
+        bool alive = true;
+    void eat() {
+        std::cout << "This animal is eating\n";
     }
+};
+class Dog : public Animal {
+    public:
 
-    int getTemperature() {
-        return temperature;
+    void bark() {
+        std::cout << "The dog goes woof!\n";
     }
-    void setTemperature(int temperature) {
-        if(temperature < 0) {
-            this -> temperature = 0;
-        }
-        else if(temperature >= 10) {
-            this -> temperature = 10;
-        } else {
-            this -> temperature = temperature;
-        }
+};
+class Cat : public Animal {
+    public:
+
+    void meow() {
+        std::cout << "The cat goes meow!\n";
+    }
+};
+
+
+class Shape {
+    public:
+        double area;
+        double volume;
+};
+class Cube : public Shape {
+    public:
+        double side;
+    Cube(double side) {
+        this -> side = side;
+        this -> area = side * side * 6;
+        this -> volume = side * side * side;
+    }
+};
+class Sphere : public Shape {
+    public:
+        double radius;
+    Sphere(double radius) {
+        this -> radius = radius;
+        this -> area = 4 * 3.14159 * (radius * radius);
+        this -> volume = (4/3.0) * 3.14159 * (radius * radius * radius);
     }
 };
 
 int main() {
-    Stove stove(0);
-    
-    //stove.setTemperature(5);
+    Dog dog;
+    Cat cat;
+    Cube cube(10);
+    Sphere sphere(5);
 
-    std::cout << "The temperature setting is: " << stove.getTemperature();
+    std::cout << "Area: " << cube.area << "cm\n";
+    std::cout << "Volume: " << cube.volume << "cm\n";
+
+    std::cout << "Area: " << sphere.area << "cm\n";
+    std::cout << "Volume: " << sphere.volume << "cm\n";
+
+    //std::cout << dog.alive << '\n';
+    //dog.eat();
+    //dog.bark();
+    
+    //std::cout << cat.alive << '\n';
+    //cat.eat();
+    //cat.meow();
+
     return 0;
 }
